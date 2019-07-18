79f2733edb50a9bee246d77a735a0628278fa51e
Ted Jacobs <doldrone@bellsouth.net>
2019-07-18 19:03:22 -0400

    Initial save.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    retturn (
+      <div>
+        //add components here
+      </div>
+    )
+  }
+}
+
+export default App;
