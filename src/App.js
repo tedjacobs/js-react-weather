2dee1063e4827bb46316a93250cdeadf11f0e88c
Ted Jacobs <doldrone@bellsouth.net>
2019-07-20 01:26:11 -0400

    Added one api key

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const Api_Key = "15f7fdb47aa5b5cdfe330b630aa36a04"
 
 class App extends React.Component {
+
+  // getWeather method to call api
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=louisville,ky&appid=${Api_Key}`);
+    const response = await api_call.json();
+    console.log(response);
+  }
+  
   render() {
     return(
       <div>
         <Titles />
-        <Form />
+        <Form loadWeather={this.getWeather} />
         <Weather />  
       </div>
     )
@@ -17,3 +27,4 @@ class App extends React.Component {
 }
 
 export default App;
+
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Form = (props) => {
-    return (
-        <form onSubmit={props.loadWeather}>
-            <input type="text" name="city" placeholder="City..." />
-            <input type="text" name="country" placeholder="Country..." />
-            <button>Get Weather</button>
-        </form>
-    )
+const Form extends React.Component{
+    render() {
+        return (
+            <form onSubmit = {this.props.loadWeather}>
+                <input type="text" name="city" placeholder="City..." />
+                <input type="text" name="country" placeholder="Country..." />
+                <button>Get Weather</button>
+            </form>
+        )
+    }
 }
 
 export default Form;
